43103dbaaeb61343681c28ba4dcb3c36f8199714
Nicole Slater <nicoleslater@pursuit.org>
2023-09-16 05:48:12 -0400

    Updating App.jsx and Route for ImageResult.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import {BrowserRouter as Router, Route, Routes} from react-router-dom
-
+import ImageResult from './components/common/ImageResult'
 import Home from './components/Home/Home'
 import Favorites from './components/Favorites/Favorites'
 import ImageDetails from './components/ImageDetails/ImageDetails'
@@ -19,7 +19,7 @@ function App() {
               <Route path='/favorites' element={<Favorites/>}/>
               <Route path='/imagedetails/:id' element={<ImageDetails/>}/>
               <Route path='/search/:query' element={<Home/>}/>
-
+              <Route path='/imageresult' element={<ImageResult/>}/>
             </Routes>
           </main>
         </Router>
