ef6dddad26b0c2a9173743322cdfb8282ed3b5e2
Michael Kleemoff <michaelkleemoff2@gmail.com>
2023-09-17 12:57:51 -0400

    Add grid to 'About.css'. Add width, height to images, border, border radius in 'AboutEach.css'.

--- src/components/AboutEach.jsx
+++ src/components/AboutEach.jsx
@@ -1,5 +1,17 @@
-const AboutEach = () => {
-	return <div>AboutEach</div>;
+/* eslint-disable react/prop-types */
+import './AboutEach.css';
+
+const AboutEach = ({ fellow }) => {
+	const { name, profilePic, github, linkedin } = fellow;
+
+	return (
+		<li className='fellow'>
+			<img src={profilePic} alt={name} />
+			<div>
+				<h3>{name}</h3>
+			</div>
+		</li>
+	);
 };
 
 export default AboutEach;
