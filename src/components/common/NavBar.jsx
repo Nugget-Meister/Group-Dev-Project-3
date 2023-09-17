3da870d5cfc47440d8c4c513d6e8ab4c36615a47
DaviiJones <davonbridgett@pursuit.org>
2023-09-17 16:16:45 -0400

    NavBar Button

--- src/components/common/NavBar.jsx
+++ src/components/common/NavBar.jsx
@@ -1,43 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 
-function NavBar () {
+    function NavBar() {
+        const [navBarClosed, navBarOpen] = useState(false);
 
-    return (
-        <nav className="navbar navbar-dark bg-dark fixed-top">
-            <div className="container-fluid">
-                <a className="navbar-brand" href="#">Offcanvas dark navbar</a>
-                <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="offcanvas offcanvas-end text-bg-dark" tabIndex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
-                    <div className="offcanvas-header">
-                        <h5 className="offcanvas-title" id="offcanvasDarkNavbarLabel">Menu</h5>
-                        <button type="button" className="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-                    </div>
-                    <div className="offcanvas-body">
-                        <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
-                            <li className="nav-item">
-                                <a className="nav-link active" aria-current="page" href="#">About Us</a>
-                            </li>
-                        </ul>
-                    </div>
-                    <div className="offcanvas-body">
-                        <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
-                            <li className="nav-item">
-                                <a className="nav-link active" aria-current="page" href="#">New Search</a>
-                            </li>
-                        </ul>
-                    </div>
-                    <div className="offcanvas-body">
-                        <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
-                            <li className="nav-item">
-                                <a className="nav-link active" aria-current="page" href="#">Favorites</a>
-                            </li>
-                        </ul>
+        const toggleOffNav = () => {
+            navBarOpen(!navBarClosed);
+        };
+        return (
+            <nav className="navbar navbar-dark bg-dark fixed-top">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="#">Metropolitan Museum of Art</a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="offcanvas offcanvas-end text-bg-dark" tabIndex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
+                        <div className="offcanvas-header">
+                            <h5 className="offcanvas-title" id="offcanvasDarkNavbarLabel">Menu</h5>
+                            <button type="button" className="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                        </div>
+                        <div className="offcanvas-body">
+                            <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
+                                <li className="nav-item">
+                                    <a className="nav-link active" aria-current="page" href="#">About Us</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div className="offcanvas-body">
+                            <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
+                                <li className="nav-item">
+                                    <a className="nav-link active" aria-current="page" href="#">New Search</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div className="offcanvas-body">
+                            <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
+                                <li className="nav-item">
+                                    <a className="nav-link active" aria-current="page" href="#">Favorites</a>
+                                </li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </nav>
-    )
-}
-export default NavBar;
+            </nav>
+        )
+    }
+    export default NavBar;
