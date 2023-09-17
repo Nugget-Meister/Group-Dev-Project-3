57fdbd737226dc17614812003be9753c436a8c4f
Michael Kleemoff <michaelkleemoff2@gmail.com>
2023-09-17 12:33:14 -0400

    Create 'AboutEach.jsx' child component. Add 'About Us' h2. Import 'uuid'. Center h2. Add 'map' to 'fellows' and 'AboutEach.jsx'.

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,21 @@
+import fellowsData from '../../data/fellows.json';
+import AboutEach from './AboutEach';
+import './About.css';
+import { v4 as uuidv4 } from 'uuid';
+
 const About = () => {
-	return <div>About</div>;
+	const fellows = fellowsData;
+
+	return (
+		<div className='about'>
+			<h2>About Us</h2>
+			<ul className='fellows'>
+				{fellows.map((fellow) => (
+					<AboutEach key={uuidv4()} fellow={fellow} />
+				))}
+			</ul>
+		</div>
+	);
 };
 
 export default About;
---
+++ src/components/AboutEach.jsx
@@ -0,0 +1,5 @@
+const AboutEach = () => {
+	return <div>AboutEach</div>;
+};
+
+export default AboutEach;
