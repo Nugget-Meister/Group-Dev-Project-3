7673c9c5e4347fee504a4f0964ad426a114f3fef
Nugget-Meister <tremainecamacho@gmail.com>
2023-09-17 16:16:00 -0400

    fixed errant edit on vite config, npm install afterwards

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
- resolve: {
-  alias:{
-    '/@assets/': path.resolve(__dirname, 'src/assets/'),  
-  },
- },
 });
