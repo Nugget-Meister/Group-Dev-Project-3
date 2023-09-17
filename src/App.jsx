e39758acd1b334a35aa2b2d228cdd26510d03519
Michael Kleemoff <michaelkleemoff2@gmail.com>
2023-09-17 09:32:55 -0400

    Create 'About.jsx' file and add route to 'App.jsx'.

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,29 @@
-import {BrowserRouter as Router, Route, Routes} from react-router-dom
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-import Home from './components/Home/Home'
-import Favorites from './components/Favorites/Favorites'
-import ImageDetails from './components/ImageDetails/ImageDetails'
-import Results from './components/Results/Results'
+import Home from './components/Home/Home';
+import Favorites from './components/Favorites/Favorites';
+import ImageDetails from './components/ImageDetails/ImageDetails';
+import About from './components/About';
+import Results from './components/Results/Results';
 
-import './App.css'
+import './App.css';
 
 function App() {
-
-  return (
-  
-      <div className='wrapper'>
-        <Router>
-          <main>
-            <Routes>
-              <Route path='/' element={<Home/>}/>
-              <Route path='/favorites' element={<Favorites/>}/>
-              <Route path='/imagedetails/:id' element={<ImageDetails/>}/>
-              <Route path='/search/:query' element={<Home/>}/>
-
-            </Routes>
-          </main>
-        </Router>
-      </div>
-    
-  )
+	return (
+		<div className='wrapper'>
+			<Router>
+				<main>
+					<Routes>
+						<Route path='/' element={<Home />} />
+						<Route path='/about' element={<About />} />
+						<Route path='/favorites' element={<Favorites />} />
+						<Route path='/imagedetails/:id' element={<ImageDetails />} />
+						<Route path='/search/:query' element={<Home />} />
+					</Routes>
+				</main>
+			</Router>
+		</div>
+	);
 }
 
-export default App
+export default App;
