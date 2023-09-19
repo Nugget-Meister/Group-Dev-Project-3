cf528e626de5bfc6369f7aefaa15fdfcea4802e0
DaviiJones <davonbridgett@pursuit.org>
2023-09-19 08:32:01 -0400

    image details start

--- src/components/ImageDetails/ImageDetails.jsx
+++ src/components/ImageDetails/ImageDetails.jsx
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, {useState, useEffect, useReducer} from 'react';
 
-const ImageDetails = () => {
+const ImageDetails = (URL,objectID) => {
     return (
         <div className='ImageDetails'>
+            <h2>{image.title}</h2>
+            <img
+                src={URL}
+                alt={`Object ${objectID}`}
+            />
+            <h2>Categories</h2>
             
+            <p>{image.description}</p>
+
+
         </div>
     );
 }
