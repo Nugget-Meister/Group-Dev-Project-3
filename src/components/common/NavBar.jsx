d7cead936cd1e2651f0c2a596865c8e514444267
DaviiJones <davonbridgett@pursuit.org>
2023-09-19 01:39:13 -0400

    navbar 100% red

--- src/components/common/NavBar.jsx
+++ src/components/common/NavBar.jsx
@@ -15,11 +15,11 @@ function NavBar() {
     return (
         <nav className="navbar navbar-dark bg-danger fixed-top">
             <div className="container-fluid">
-                <a className="navbar-brand" href="http://localhost:5173/">Metropolitan Museum of Art</a>
+                <a className="navbar-brand" href="http://localhost:5173/">Museum Mate</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
-                <div className="offcanvas offcanvas-end text-bg-dark" tabIndex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
+                <div className="offcanvas offcanvas-end text-bg-danger" tabIndex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
                     <div className="offcanvas-header">
                         <h5 className="offcanvas-title" id="offcanvasDarkNavbarLabel">Menu</h5>
                         <button type="button" className="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
