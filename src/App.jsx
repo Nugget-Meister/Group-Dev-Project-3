e14a21de555e1c8459d2662abebd9148d91f462c
Nicole Slater <nicoleslater@pursuit.org>
2023-09-19 16:55:04 -0400

    Updated CSS for Mobile Screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 
 import {useState} from "react"
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom"
-import ImageResult from './components/common/ImageResult'
+// import ImageResult from './components/common/ImageResult'
 
 import Home from './components/Home/Home'
 import Favorites from './components/Favorites/Favorites'
